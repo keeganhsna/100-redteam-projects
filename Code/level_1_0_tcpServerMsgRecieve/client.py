0807d4816695debf6ad6bd52e896b58983cd694d
keeganhsna <32498790+keeganhsna@users.noreply.github.com>
2023-03-19 15:00:01 -0700

    updating my code

---
+++ Code/level_1_0_tcpServerMsgRecieve/client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+        print(client.recv(2048).decode(FORMAT))
+
+def listen():
+        tf = True
+        while tf:
+            message = input()
+            if (message != DISCONNECT_MESSAGE):
+                    send(message)
+            else:
+                    send(DISCONNECT_MESSAGE)
+                    break
+
+listen()
